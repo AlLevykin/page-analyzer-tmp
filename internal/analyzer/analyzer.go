08d70eacc4b806c44bba7bfa15dcde9f13c191b3
Алексей Левыкин <a.a.levykin@gmail.com>
2022-08-20 08:15:20 +0500

    architecture

---
+++ internal/analyzer/analyzer.go
@@ -0,0 +1,16 @@
+package analyzer
+
+import (
+	"context"
+)
+
+type Analyzer struct {
+}
+
+func NewAnalyzer(urls []string) *Analyzer {
+	return nil
+}
+
+func (a *Analyzer) Process(ctx context.Context) {
+
+}
---
+++ internal/urls/urls.go
@@ -0,0 +1,5 @@
+package urls
+
+func Parse(s string) ([]string, error) {
+	return nil, nil
+}
